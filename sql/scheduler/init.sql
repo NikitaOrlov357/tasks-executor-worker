a856082ec7c19f98d7f04b702e9f97d5bb89c3b2
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-01-12 14:57:51 +0300

    refactor

--- sql/scheduler/init.sql
+++ sql/scheduler/init.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE scheduler;
 
 CREATE TABLE commands(
-     id serial NOT NULL, --уникальный номер записи
-     command VARCHAR NOT NULL, --команда для выполнения
-     type VARCHAR NOT NULL, --тип команды
-     regularity BIGINT NOT NULL, --регулярность выполнения в секундах
-     start BIGINT NOT NULL,--время начала работы "UNIX_TIME"
-     trigger VARCHAR NOT NULL,--кто создал задачу
-     time TIMESTAMP NOT NULL --время создания задачи
+                         id         serial    NOT NULL, -- уникальный номер записи
+                         command    VARCHAR   NOT NULL, -- команда для выполнения
+                         type       VARCHAR   NOT NULL, -- тип команды
+                         regularity BIGINT    NOT NULL, -- регулярность выполнения в секундах
+                         start      BIGINT    NOT NULL, -- время начала работы "UNIX_TIME"
+                         trigger    VARCHAR   NOT NULL, -- кто создал задачу
+                         time       TIMESTAMP NOT NULL  -- время создания задачи
 )

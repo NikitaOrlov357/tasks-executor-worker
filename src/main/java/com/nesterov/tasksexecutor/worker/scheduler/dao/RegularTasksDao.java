923d78b1629600bc472f59966dae04371cab8a8b
BabyBurrito <mywketep00@mail.ru>
2022-01-16 11:08:48 +0300

    добавили зависимости

--- src/main/java/com/nesterov/tasksexecutor/worker/scheduler/dao/RegularTasksDao.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/scheduler/dao/RegularTasksDao.java
@@ -2,14 +2,10 @@ package com.nesterov.tasksexecutor.worker.scheduler.dao;
 
 import com.nesterov.tasksexecutor.worker.scheduler.dao.interfaces.CommandsDao;
 import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository

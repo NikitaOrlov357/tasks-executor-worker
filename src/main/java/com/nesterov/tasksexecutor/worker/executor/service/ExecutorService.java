d574cfa75120738c4708d4090310dbaecfdd3d40
BabyBurrito <mywketep00@mail.ru>
2022-02-06 12:06:24 +0300

    добавили выбор runner-а

--- src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorService.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorService.java
@@ -2,15 +2,36 @@ package com.nesterov.tasksexecutor.worker.executor.service;
 
 import com.nesterov.tasksexecutor.worker.executor.service.runners.CmdRunner;
 import com.nesterov.tasksexecutor.worker.executor.service.runners.Runner;
+import com.nesterov.tasksexecutor.worker.executor.utils.CommandTypes;
+import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExecutorService {
 
-    Runner runner;
+    Runner cmdRunner;
 
-    public ExecutorService(Runner runner){
-        this.runner = runner;
+    public ExecutorService(Runner cmdRunner){
+        this.cmdRunner = cmdRunner;
+    }
+
+    public void execute(Command command){
+        Runner runner = getRunner(command);
+        runner.run(command);
+    }
+
+    @Nullable
+    private Runner getRunner (Command command){
+        switch (command.getType()){
+            case CommandTypes.CMD:
+                return cmdRunner;
+
+            case CommandTypes.BASH:
+                return null;
+            default:
+                return null;
+        }
     }
 
 }

3c59a96819410604cb2c6697ff9f3826f5bb3065
Vahza <waja11@mail.ru>
2022-03-10 20:56:59 +0300

    Добавил Таймер для измерения времени выполнения задачи

---
+++ src/main/java/com/nesterov/tasksexecutor/worker/utils/Timer.java
@@ -0,0 +1,30 @@
+package com.nesterov.tasksexecutor.worker.utils;
+public  class Timer {
+    private long start;
+    private long finish;
+
+    public void start() {
+        start = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        finish = System.currentTimeMillis();
+    }
+
+    public long getTime() {
+
+        return finish - start;
+    }
+
+    //Timer для сигнатыры Run
+    public interface MethodWithoutParams{
+        void doMethod();
+    }
+    public static long doAndGetTime (MethodWithoutParams methodWithoutParams ){
+        long start = System.currentTimeMillis();
+        methodWithoutParams.doMethod();
+        long finish = System.currentTimeMillis();
+        return finish - start;
+    }
+
+}

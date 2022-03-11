af6ffb7c517e08dc0a98e30acbd871839d67f789
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-03-11 17:39:41 +0300

    Revert "Feature/timer"

--- src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorService.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorService.java
@@ -4,7 +4,6 @@ package com.nesterov.tasksexecutor.worker.executor.service;
 import com.nesterov.tasksexecutor.worker.executor.runners.implementations.CmdRunner;
 import com.nesterov.tasksexecutor.worker.executor.runners.Runner;
 import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
-import com.nesterov.tasksexecutor.worker.utils.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class ExecutorService {
+
     Runner cmdRunner;
 
     public ExecutorService(CmdRunner cmdRunner){
@@ -22,7 +22,7 @@ public class ExecutorService {
         Runner runner = getRunner(command);
         if (runner != null) {
             ExecutorThread executorThread = new ExecutorThread(runner, command);
-            long timeOfMethod = Timer.doAndGetTime(executorThread::start);
+            executorThread.start();
         }
         else {
             log.error("runner was not found for command = {}", command);

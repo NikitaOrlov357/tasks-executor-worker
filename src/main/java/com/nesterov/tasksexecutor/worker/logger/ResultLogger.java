34bfcc609626d4435a00687580939ee07e0e856c
vahzatsaava <waja11@mail.ru>
2022-03-19 19:59:22 +0300

    вывел else в отдельный метод

--- src/main/java/com/nesterov/tasksexecutor/worker/logger/ResultLogger.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/logger/ResultLogger.java
@@ -1,5 +1,7 @@
 package com.nesterov.tasksexecutor.worker.logger;
 
+import com.nesterov.tasksexecutor.worker.executor.service.ExecutorService;
+import com.nesterov.tasksexecutor.worker.utils.timer.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,10 +26,13 @@ public class ResultLogger {
         if (resultLoggerDummyModeEnable){
             log.info("dummyMode for result logger command={}, result={}, message={}, owner={}, start={}, duration={}", command, result, message, owner, start, duration);
         } else{
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(hikariDataSource);
-            String sql = "INSERT INTO log (command, result, message, owner, start, duration) VALUES (?, ?, ?, ?, ?, ?)";
-            jdbcTemplate.update(sql, command, result, message, owner, start, duration);
+           logDefault(command,result,message,owner,start,duration);
         }
+    }
+    public void logDefault(String command, boolean result,String message, String owner ,Date start, long duration){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(hikariDataSource);
+        String sql = "INSERT INTO log (command, result, message, owner, start, duration) VALUES (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, command, result, message, owner, start, duration);
     }
 
 }

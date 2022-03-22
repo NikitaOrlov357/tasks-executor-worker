fb4f60984b61907b3f1103a1d0ec6dd120571f85
vahzatsaava <waja11@mail.ru>
2022-03-22 17:46:46 +0300

    вывел else в отдельный метод

---
+++ src/main/java/com/nesterov/tasksexecutor/worker/controllers/SchedulerController.java
@@ -0,0 +1,16 @@
+package com.nesterov.tasksexecutor.worker.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("scheduler")
+public class SchedulerController {
+@PostMapping("/stop")
+    public String stopScheduler(){
+    return "stop";
+}
+
+}

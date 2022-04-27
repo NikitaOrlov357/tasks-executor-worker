2976340d85ade15323956fb6a0d54cf34c1d425a
vahzatsaava <waja11@mail.ru>
2022-04-27 22:04:00 +0300

    join всех таблиц

--- src/main/java/com/nesterov/tasksexecutor/worker/scheduler/dao/implementations/RegularTasksDbDao.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/scheduler/dao/implementations/RegularTasksDbDao.java
@@ -44,6 +44,7 @@ public class RegularTasksDbDao implements CommandsDao {
                             rs.getLong("start"),
                             rs.getString("owner"),
                             rs.getDate("time")
+
                     )
         );
     }

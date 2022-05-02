26bb0ee9efb98c7dd29c80f1393e62e3ea5d5156
vahzatsaava <waja11@mail.ru>
2022-05-02 21:11:10 +0300

    добавили FutureTusk изменили архитектуру выполнения задачи

---
+++ src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorFutureTask.java
@@ -0,0 +1,15 @@
+package com.nesterov.tasksexecutor.worker.executor.service;
+
+import com.nesterov.tasksexecutor.worker.executor.runners.Result;
+import com.nesterov.tasksexecutor.worker.executor.runners.Runner;
+import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class ExecutorFutureTask extends FutureTask<Result> {
+
+    public ExecutorFutureTask(Runner runner, Command command){
+        super(() -> runner.run(command));
+    }
+}
--- src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorThread.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorThread.java
@@ -1,20 +1,38 @@
 package com.nesterov.tasksexecutor.worker.executor.service;
 
+import com.nesterov.tasksexecutor.worker.executor.runners.Result;
 import com.nesterov.tasksexecutor.worker.executor.runners.Runner;
 import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
-public class ExecutorThread extends Thread{
-    private final Runner runner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+@Slf4j
+public class ExecutorThread extends Thread {
     private final Command command;
+    private final Runner runner;
 
-    public ExecutorThread(Runner runner, Command command){
-        this.runner = runner;
+    public ExecutorThread(Command command, Runner runner) {
         this.command = command;
+        this.runner = runner;
     }
 
 
     @Override
     public void run() {
-        runner.run(command);
+        ExecutorFutureTask executorFutureTask = new ExecutorFutureTask(runner, command);
+        new Thread(executorFutureTask).start();
+        Result result = null;
+        try {
+            result = executorFutureTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        if (result != null) {
+            log.info("command = {}, success = {} ", command, result.isSuccess());
+            //resultLogger.log(command.getCommand(), result.isSuccess(), result.getMessage(), command.getOwner(), date, 121241124);
+        }
+
     }
 }

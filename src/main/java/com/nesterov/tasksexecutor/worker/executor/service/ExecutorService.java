f1d2b131f806a8b48c8b8b38600eadb70315bb52
vahzatsaava <waja11@mail.ru>
2022-05-06 17:05:46 +0300

    timeOfMethod переписал

--- src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorService.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/executor/service/ExecutorService.java
@@ -2,6 +2,8 @@ package com.nesterov.tasksexecutor.worker.executor.service;
 
 import com.nesterov.tasksexecutor.worker.executor.runners.Runner;
 import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
+import com.nesterov.tasksexecutor.worker.utils.timer.TimeUnit;
+import com.nesterov.tasksexecutor.worker.utils.timer.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,7 @@ public class ExecutorService {
         Runner runner = runnerSwitchService.getRunner(command);
         if (runner != null) {
             new ExecutorThread(command, runner).start();
-           // long timeOfMethod = Timer.doAndGetTime(executorFutureTask::start, TimeUnit.MILLISECONDS);
+            long timeOfMethod = Timer.doAndGetTime(new ExecutorThread(command,runner)::start, TimeUnit.MILLISECONDS);
         }
         else {
             log.error("runner was not found for command = {}", command);

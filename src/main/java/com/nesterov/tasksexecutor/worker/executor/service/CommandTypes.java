40259feb4cbde8b3638a0a898d5674ead59c8db7
BabyBurrito <mywketep00@mail.ru>
2022-05-08 11:53:15 +0300

    заменили interface на enum

--- src/main/java/com/nesterov/tasksexecutor/worker/executor/service/CommandTypes.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/executor/service/CommandTypes.java
@@ -1,6 +1,6 @@
 package com.nesterov.tasksexecutor.worker.executor.service;
 
-public interface CommandTypes {
-    String CMD = "cmd";
-    String BASH = "bash";
+public enum CommandTypes {
+    CMD,
+    BASH
 }
--- src/main/java/com/nesterov/tasksexecutor/worker/scheduler/dao/implementations/RegularTasksDbDao.java
+++ src/main/java/com/nesterov/tasksexecutor/worker/scheduler/dao/implementations/RegularTasksDbDao.java
@@ -1,6 +1,7 @@
 package com.nesterov.tasksexecutor.worker.scheduler.dao.implementations;
 
 import com.nesterov.tasksexecutor.worker.configs.applicationConfigs.ExternalConfigs;
+import com.nesterov.tasksexecutor.worker.executor.service.CommandTypes;
 import com.nesterov.tasksexecutor.worker.scheduler.dao.CommandsDao;
 import com.nesterov.tasksexecutor.worker.scheduler.dto.Command;
 
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,7 +38,7 @@ public class RegularTasksDbDao implements CommandsDao {
                     new Command(
                             rs.getInt("id"),
                             rs.getString("command"),
-                            rs.getString("type"),
+                            CommandTypes.valueOf(rs.getString("type").toUpperCase(Locale.ROOT)),
                             rs.getLong("regularity"),
                             rs.getLong("start"),
                             rs.getString("owner"),

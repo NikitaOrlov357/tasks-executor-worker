8d2bd16dd9c011bc8ae9aeb571728f78bf69a271
Aleksandr Nesterov <a.nesterov@bpcbt.com>
2022-05-20 12:16:04 +0300

    added junit5

---
+++ src/test/java/com/nesterov/tasksexecutor/worker/executor/service/RunnerSwitchServiceTest.java
@@ -0,0 +1,12 @@
+package com.nesterov.tasksexecutor.worker.executor.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RunnerSwitchServiceTest {
+
+    @Test
+    public void switchServiceTest(){
+        Assertions.assertEquals(1, 2);
+    }
+}

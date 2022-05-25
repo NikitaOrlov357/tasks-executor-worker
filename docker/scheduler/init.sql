10206cc48aee1c5dc3fa01c30ba0d151801442c9
vahzatsaava <waja11@mail.ru>
2022-05-25 19:56:24 +0300

    изменил запрос DB

--- docker/scheduler/init.sql
+++ docker/scheduler/init.sql
@@ -1,32 +1,33 @@
 DROP TABLE IF EXISTS command;
 DROP TABLE IF EXISTS command_type;
-DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS owner;
+DROP TABLE IF EXISTS log;
 
 CREATE TABLE command_type
 (
-    id SERIAL NOT NULL,--уникальный номер записи
+    id SERIAL NOT NULL PRIMARY KEY ,--уникальный номер записи
     type VARCHAR NOT NULL, -- тип команды,
-    PRIMARY KEY (id)
+
 );
 
 CREATE TABLE owner
 (
-    id SERIAL NOT NULL,--уникальный номер записи
+    id SERIAL NOT NULL PRIMARY KEY,--уникальный номер записи
     name VARCHAR NOT NULL, -- имя команды
-    PRIMARY KEY (id)
+
 
 );
 
 CREATE TABLE command
 (
-    id serial NOT NULL, -- уникальный номер записи
+    id serial NOT NULL PRIMARY KEY , -- уникальный номер записи
     command VARCHAR NOT NULL , -- команда для выполнения
     type_id INT NOT NULL REFERENCES command_type (id), -- id типа команды
     regularity BIGINT NOT NULL, -- регулярность выполнения в секундах
     start BIGINT NOT NULL, -- время начала работы "UNIX_TIME"
     owner_id INT NOT NULL REFERENCES owner (id), -- кто создал задачу
     time TIMESTAMP NOT NULL DEFAULT now(),  -- время создания задачи
-    PRIMARY KEY (id)
+
 
 );
 
@@ -37,12 +38,12 @@ VALUES ('ping yandex.ru', 1, 60, 1643300220, 1);
 
 
 CREATE TABLE log(
-    id serial NOT NULL , -- уникальный номер записи
+    id serial NOT NULL PRIMARY KEY , -- уникальный номер записи
     command_id INT NOT NULL REFERENCES command(id), -- команда для выполнения
     result BOOLEAN NOT NULL, -- результат
     message VARCHAR NOT NULL, -- сообщение
     owner VARCHAR NOT NULL, -- кто создал задачу
     start TIMESTAMP NOT NULL, -- время начала задачи
     duration BIGINT NOT NULL,  -- время длит-ти задачи (в милисек)
-    PRIMARY KEY (id)
+
 );
